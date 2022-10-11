e556f933efee07ddb1ace863aae4bdd25f4be6bf
Julian Alsemmani <juliankonge@hotmail.com>
2022-10-11 19:03:33 +0200

    Adding menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Category from "./components/Category";
 import Home from "./pages/Home";
 import Pages from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
+      <Category />
       <Pages />
     </div>
   );
