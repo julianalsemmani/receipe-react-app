9b76b100b48c5dbc910ed8eea58c87dde0eec42e
Julian Alsemmani <juliankonge@hotmail.com>
2022-10-11 09:06:17 +0200

    Added Components.css and removed the css from the react components files

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
-import styled from 'styled-components';
 import { Splide, SplideSlide } from '@splidejs/react-splide'
 import '@splidejs/splide/dist/css/splide.min.css';
+import '../components.css';
 
 export default function Popular() {
 
@@ -27,7 +27,7 @@ export default function Popular() {
 
   return (
     <div>
-      <Wrapper>
+      <div className='wrapper'>
         <h3>Popular Picks</h3>
         <Splide options={{
           perPage: 4,
@@ -39,62 +39,15 @@ export default function Popular() {
           {popular.map((recipe) => {
             return (
               <SplideSlide key={recipe.id}>
-                <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                  <Gradient />
-                </Card>
+                <div className='card'>
+                  <p className='card-text'>{recipe.title}</p>
+                  <img className='card-img' src={recipe.image} alt={recipe.title} />
+                </div>
               </SplideSlide>
-            )
+            );
           })}
         </Splide>
-      </Wrapper>
+      </div>
     </div>
   )
 }
-
-const Wrapper = styled.div`
-  margin: 4rem 0rem;
-  `;
-
-const Card = styled.div`
-  min-height: 25rem;
-  border-radius: 2rem;
-  overflow: hidden;
-  position: relative;
-
-  img {
-    border-radius: 2rem;
-    position: absolute;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    background: linear-gradient(rgba(0, 0, 0, 0) rgba(0, 0, 0, 0.5));
-  }
-
-  p {
-    position: absolute;
-    z-index: 10;
-    left: 50%;
-    bottom: 0%;
-    transform: translate(-50%, 0%);
-    color: white;
-    width: 100%;
-    text-align: center;
-    font-weight: 600;
-    font-size: 1rem;
-    height: 40%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  `;
-
-const Gradient = styled.div`
-  z-index: 3;
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(rgba(0, 0, 0, 0) rgba(0, 0, 0, 0.5));
-`;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 function Home() {
     return (
         <div>
-            <Veggie />
             <Popular />
+            <Veggie />
         </div>
     )
 }
